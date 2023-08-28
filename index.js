1943e3a6e15158b263c6e3247934bfe42e158638
Md. Mehedi Hasan Nabil <mehedihasannabil49@gmail.com>
2023-08-28 16:01:05 +0600

    create compact function

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const chunk = require("./lib/chunk")
+const compact = require("./lib/compact")
 
 const numbers = [1, 2, 3, 8, 4, 12, 7]
 
-console.log(chunk(numbers, 5))
+// console.log(chunk(numbers, 5))
+console.log(compact([0, 1, false, 2, '', 3]))
