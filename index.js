6a831abbc14cc77fbb383c345b7daea5779b7214
Md. Mehedi Hasan Nabil <mehedihasannabil49@gmail.com>
2023-08-28 17:18:44 +0600

    create concat function

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const chunk = require("./lib/chunk")
 const compact = require("./lib/compact")
+const concat = require("./lib/concat")
 
 const numbers = [1, 2, 3, 8, 4, 12, 7]
 
 // console.log(chunk(numbers, 5))
-console.log(compact([0, 1, false, 2, '', 3]))
+// console.log(compact([0, 1, false, 2, '', 3]))
+console.log(concat(2, [3], [[4]]))
